ffafeddc9c60cd0ea4649f65e56a307f73f6bf38
RusoKan <rusosingam@gmail.com>
2023-05-20 21:09:11 -0400

    Established Session, Login,Logout and connected to Client-side
        -User can login and cookies is saved in browser
        -User can also logout of the session
        -Added loading in login

--- server.js
+++ server.js
@@ -2,16 +2,71 @@ require('dotenv').config()
 
 const bodyParser = require("body-parser")
 const express = require("express")
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const app = express()
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
+const session = require('express-session');
 const saltRounds = 10;
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json());
 app.use(cors())
 PORT = process.env.PORT || 3000
+
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+}))
+
+app.use(passport.initialize())
+
+app.use(passport.session())
+
+
+
+const authUser = (email, password, done) => {
+    Account.findOne({ Email: email }).exec()
+        .then((account) => {
+            if (account) {
+                bcrypt.compare(password, account.Password).then(function (result) {
+                    if (result) {
+                        done(null, { account }, { message: "Succesfull" })
+
+                    }
+                    else {
+                        // console.log("Check  password again",)      
+                        done(null, false, { message: "Invalid password" })
+
+                    }
+                })
+            }
+            else {
+                // console.log("Check your email ",)
+                done(null, false, { message: "Invalid password" })
+            }
+
+        })
+        .catch((error) => done(error));
+
+    ;
+
+
+}
+passport.use(new LocalStrategy({
+    usernameField: 'email',    // define the parameter in req.body that passport can use as username and password
+    passwordField: 'password'
+},authUser))
+passport.serializeUser((userObj, done) => {
+    done(null, userObj)
+})
+passport.deserializeUser((userObj, done) => {
+    done(null, userObj)
+})
+
 
 const { Schema, model } = mongoose;
 const MongooseConnect = async () => {
@@ -33,38 +88,76 @@ const user_accounts = new Schema({
 })
 const Account = model("Account", user_accounts)
 
-app.get("/", (req, res, next) => {
-
-    res.sendFile(__dirname + "/index.html")
-})
 app.post("/login", (req, res, next) => {
-
-
-    Account.findOne({ Email: req.body.email }).exec()
-        .then((account) => {
-            if (account) {
-                bcrypt.compare(req.body.password, account.Password).then(function (result) {
-                    if (result) {
-                        console.log("Account:", account)
-                        res.json(true)
-                    }
-                    else {
-                        console.log("Check  password again",)
-                        res.json(false)
-
-                    }
-                })
+    passport.authenticate("local", (err, user, info) => {
+      if (user) {
+        req.logIn(user, (err) => {
+            if (err) {
+              return next(err);
             }
-            else {
-                console.log("Check your email ",)
-                res.json(false)
-            }
+        console.log(user, info);
+        console.log(req.isAuthenticated())
+        return res.json(true);
+        
+      }
+         ) 
+    }else {
+        console.log(user, info);
+        // Authentication failed
+        return res.json(false);
+      }
+    })(req, res, next);
+  });
 
-        });
+  app.get("/dashboard",(req,res,next)=>{
+    res.json(req.isAuthenticated())
 
-}
+  })
+  app.post('/logout', function(req, res, next){
+    req.logout(function(err) {
+      if (err) { return next(err); }
+      console.log(req.isAuthenticated())
+      res.json(req.isAuthenticated())
+    });
+  });
+  
+// app.post("/login", (req, res, next) => {
+//     passport.authenticate("local", (err, user, info) => {
+//       if (user) {
+//         return res.json(true);
+//       } else {
+//         console.log(user, info)
+//         // Authentication failed
+//         return res.json(false);
+//       }
+//     })(req, res, next);
+//   });
 
-)
+// app.post("/login", (req, res, next) =>
+
+// {passport.authenticate("local",(err, user, info) =>
+//         {
+//             console.log("hello")
+//             if (user) {
+//                 return res.json(true);
+//               }
+//               else 
+//               {
+//                 // Authentication failed
+//                 return res.json(false);
+//               }
+//         }
+//     )}
+// )
+// app.get("/dashboard", (req, res, next) => {
+//     if (req.isAuthenticated())
+//         console.log("I'm Here")
+//     else {
+//         res.redirect("/login")
+//     }
+
+
+// })
 
 
 
